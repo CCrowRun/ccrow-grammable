cf521def0a371bf8a5a6cd087c33327d9f3ece4b
Carrie Crow <carrie.g.crow@gmail.com>
2019-09-21 16:17:03 +0000

    Created delete tests

--- spec/controllers/grams_controller_spec.rb
+++ spec/controllers/grams_controller_spec.rb
@@ -1,6 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe GramsController, type: :controller do
+  describe "grams#destroy action" do
+    it "should allow a user to destroy grams" do
+      gram = FactoryBot.create(:gram)
+      delete :destroy, params: { id: gram.id }
+      expect(response).to redirect_to root_path
+      gram = Gram.find_by_id(gram.id)
+      expect(gram).to eq nil
+    end
+
+    it "should return a 404 message if we cannot find a gram with the specified id" do
+      delete :destroy, params: { id: "PIZZACAT" }
+      expect(response).to have_http_status(:not_found)
+    end
+  end
+
   describe "grams#update action" do
     it "should allow users to successfully update grams" do
       gram = FactoryBot.create(:gram, message: "Original value")
@@ -22,7 +37,6 @@ RSpec.describe GramsController, type: :controller do
       gram.reload
       expect(gram.message).to eq "Original value"
     end
-
   end  
 
   describe "grams#edit action" do
@@ -36,7 +50,6 @@ RSpec.describe GramsController, type: :controller do
       get :edit, params: { id: "PIZZACAT" }
       expect(response).to have_http_status(:not_found)
     end
-
   end
 
   describe "grams#show action" do
